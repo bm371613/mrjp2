c385078d3fff0765a83b267b01f823af95a19ce3
Bartosz Marcinkowski <bm371613@gmail.com>
2015-11-20 09:34:32 +0100

    Parsing in Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,25 @@
+import Control.Monad (unless)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+
+import Parser.AbsLatte
+import Parser.LexLatte
+import Parser.ParLatte
+import Parser.ErrM
+
 main :: IO ()
-main = return ()
+main = do
+    args <- getArgs
+    unless (not $ null args) $ do
+        hPutStrLn stderr "Missing inputFileName argument."
+        exitFailure
+    let inputFileName = head args
+    input <- readFile inputFileName
+    program <- case pProgram $ myLexer input of
+        Bad e -> do
+            hPutStrLn stderr e
+            exitFailure
+        Ok tree -> return tree
+    return ()
 
