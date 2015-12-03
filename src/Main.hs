f2a8d5e865c9b23bfb7454e5526e6a7903b4865f
Bartosz Marcinkowski <bm371613@gmail.com>
2015-12-03 20:18:00 +0100

    applied to specification

--- src/Main.hs
+++ src/Main.hs
@@ -10,22 +10,23 @@ import Parser.LexLatte
 import Parser.ParLatte
 import Parser.ErrM
 
+err :: String -> IO a
+err msg = do
+    hPutStrLn stderr "ERROR"
+    hPutStrLn stderr msg
+    exitFailure
+
 main :: IO ()
 main = do
     args <- getArgs
-    unless (not $ null args) $ do
-        hPutStrLn stderr "Missing inputFileName argument."
-        exitFailure
+    unless (not $ null args) $ err "Missing inputFileName argument."
     let inputFileName = head args
     input <- readFile inputFileName
     program <- case pProgram $ myLexer input of
-        Bad e -> do
-            hPutStrLn stderr e
-            exitFailure
+        Bad e -> err e
         Ok tree -> return tree
     globals <- case checkProgramReturningGlobals program of
-        Left error -> do
-            hPutStrLn stderr error
-            exitFailure
+        Left e -> err e
         Right globals -> return globals
     emitProgramGivenGlobals program globals
+    hPutStrLn stderr "OK"
